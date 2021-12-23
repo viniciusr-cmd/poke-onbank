32d0a1b6e360237fa72942fd70f5143d92905da9
viniciusr-cmd <vrs122@gmail.com>
2021-12-23 16:43:51 -0400

    updated main.ts swagger/open api description

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     .setTitle('Poke Onbank')
     .setDescription(
       `Poke API onbank, abaixo está os endpoints que podem ser realizados buscas e consumos, baseados na pokeapi, utilizando-se da mesma via @nestjs/axios.
-  Para consultas na PokeAPI, foi criado o endpoint /pokeapi, para consultas na custom database, fora utilizado o endpoint /pokes. Mesmo assim, peço que utilizem o INSOMNIA ou POSTMAN para uma experiência mais completa.`,
+  Para consultas na PokeAPI, foi criado o endpoint /pokeapi. Para consultas na custom database, fora utilizado o endpoint /pokes. Mesmo assim, peço que utilizem o INSOMNIA ou POSTMAN para uma experiência mais completa.`,
     )
     .build();
 
