74f061e7d3cc8465d311e56cdb009a1468136095
viniciusr-cmd <vrs122@gmail.com>
2021-12-22 20:24:44 -0400

    main.ts port env updated

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+let PORT = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api')
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(PORT);
 }
 bootstrap();
